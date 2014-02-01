7f2f271c56fa076a1e73f39cef2816ab160e60dc
edsilv <e.silverton@gmail.com>
2014-02-01 23:22:29 +0000

    initial commit

---
+++ cadeteditor.com/App_Start/BundleConfig.cs
@@ -0,0 +1,30 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace cadeteditor.com
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            //            "~/Scripts/jquery-{version}.js"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+            //            "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
+                "~/bower_components/jquery/jquery.min.js",
+                "~/bower_components/bootstrap/dist/js/bootstrap.js",      
+                "~/Scripts/respond.js"));
+
+            bundles.Add(new StyleBundle("~/bundles/css").Include(
+                      "~/bower_components/bootstrap/dist/css/bootstrap.css",
+                      "~/bower_components/bootstrap/dist/css/bootstrap-theme.css",
+                      "~/Content/site.css"));
+        }
+    }
+}
---
+++ cadeteditor.com/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="cadeteditor.com.MvcApplication" Language="C#" %>
