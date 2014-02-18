80c6ee955067b24921c67376bb94c82a53474e7b
edsilv <e.silverton@gmail.com>
2014-02-18 00:15:06 +0000

    looping carousel gifs

---
+++ cadeteditor.com/Scripts/carousel.js
@@ -0,0 +1,44 @@
+﻿var $carousel, $item, timer;
+
+$(function () {
+            
+    $carousel = $('#carousel');
+
+    $carousel.on('slide.bs.carousel', function () {
+        clearInterval(timer);
+    });
+
+    $carousel.on('slid.bs.carousel', function() {
+        updateGifs();
+    });
+
+    updateGifs();
+});
+
+function updateGifs() {
+
+    if ($item) {
+        // set the previous item (if any) to looping
+        swapGifs($item);
+    } else {
+        // this is the first img, so show it.
+        $($carousel.find('.item.active img')[0]).show();
+    }
+
+    $item = $carousel.find('.item.active');
+
+    if (!$item.data('looping')) {
+        var loopAt = parseInt($item.data('loop-at'));
+        
+        timer = setTimeout(function() {
+            swapGifs($item);
+            clearInterval(timer);
+        }, loopAt);
+    }
+}
+
+function swapGifs($item) {
+    $($item.find('img')[0]).hide();
+    $($item.find('img')[1]).show();
+    $item.data('looping', true);
+}
